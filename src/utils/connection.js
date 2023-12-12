7d1a55d2d72a0a74cd8caf5013a45ddf0eb66c6e
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2023-12-12 15:16:20 -0500

    modificaciones

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,21 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  dialectOptions: {
+    ssl: {
+      required: false,
+    },
+  },
+  define: {
+    timestamps: false,
+  },
+})
 
 module.exports = sequelize;
